65886ecc5690f0a949250e530f838c9a5307768a
Nicholas Minor <nrminor@wisc.edu>
2024-01-28 22:03:04 -0600

    adding julia script to rapidly convert vcfs to mpgls

--- bin/vcf2mpgl.R
+++ bin/vcf2mpgl.R
@@ -4,130 +4,107 @@
 
 
 #### ENVIRONMENT SETUP ####
-require(vcfR)
-require(MASS)
+library(vcfR)
+library(MASS)
+library(dplyr)
+library(tidyr)
+library(stringr)
 
 
-#### FUNCTION SETUP #### 
-
-## multi-file vcf-to-mpgl conversion function
+## helper function to convert each Phred-scaled likelihood 
 #--------------------------------------------------------------------#
-multifile_convert <- compiler::cmpfun(function(args, a.pl, a.loci, a.ids){
-  
-  ploidy1 <- as.character(args[2])
-  vcffile2 <- as.character(args[3])
-  ploidy2 <- as.character(args[4])
-  
-  fname2 <- strsplit(vcffile2, ".vcf")
-  a.vcf2 <- read.vcfR(vcffile2)
-  
-  a.ids2 <- colnames(a.vcf2@gt)[-1]
-  write.table(a.ids2, file=paste0("inds_", fname2, ".txt"),
-              row.names = FALSE, col.names = FALSE)
-  
-  a.pl2 <- extract.gt(a.vcf2, element='PL')
-  a.pl2[is.na(a.pl2)] <- paste(rep("0", ploidy2+1), collapse=", ")
-  
-  a.loci2 <- paste0(a.vcf2@fix[, 'CHROM'], ';', a.vcf2@fix[, 'POS'])
-  
-  nind <- dim(a.pl)[2]+dim(a.pl2)[2]
-  commonloci <- a.loci %in% a.loci2
-  nloci <- dim(a.pl)[1]+dim(a.pl2)[1]-length(commonloci.idx)
-  
-  total.pl <- matrix("0", nrow=nloci, ncol=nind)
-  total.pl[1:length(commonloci), 1:dim(a.pl)[2]] <- a.pl[commonloci, ]
-  total.pl[(length(commonloci)+1):dim(a.pl)[1],
-           1:dim(a.pl)[2]] <- a.pl[!commonloci, ]
-  
-  commonloci <- a.loci2 %in% a.loci
-  total.pl[1:length(commonloci),
-           (dim(a.pl)[2]+1):nind] <- a.pl2[commonloci, ]
-  total.pl[(dim(a.pl)[1]+1):nloci,
-           (dim(a.pl)[2]+1):nind] <- a.pl2[!commonloci, ]
-  
-  total.pl[(length(commonloci)+1):dim(a.pl)[1],
-           (dim(a.pl)[2]+1):nind] <- paste(rep("0", ploidy+1), collapse=", ")
-  total.pl[(dim(a.pl)[1]+1):nloci,
-           1:dim(a.pl)[2]] <- paste(rep("0", ploidy2+1), collapse=", ")
-  
-  total.pl <- apply(total.pl, c(1, 2),
-                    function(x){as.numeric(unlist(strsplit(x, split=", ")))})
-  
-  print(paste0("Number of inds: ", nind, "    Number of loci: ", nloci)) 
-  
-  loci.names <- a.loci[a.loci %in% a.loci2]
-  loci.names <- append(a.loci[!(a.loci %in% a.loci2)], loci.names)
-  loci.names <- append(a.loci2[!(a.loci2 %in% a.loci)], loci.names)
-  
-  unlink("combined.mpgl")
-  cat(paste(ncol(total.pl), nrow(total.pl), "\n"), file="combined.mpgl")
-  cat(paste(a.ids), file="combined.mpgl", append=T)
-  for(locus in 1:nloci){
-    cat(paste0("\n", loci.names[locus], " "), file="combined.mpgl",
-        append = TRUE)
-    write.table(unlist(total.pl[locus, ]), file="combined.mpgl", 
-                append = TRUE, col.names = FALSE, row.names = FALSE, eol=" ")
-  }
-  
+to_numvec <- compiler::cmpfun(function(item) {
+  return(
+    item %>%
+      strsplit(split = ",") %>%
+      unlist() %>%
+      trimws %>%
+      as.numeric()
+  )
 })
 #--------------------------------------------------------------------#
 
 
 ## vcf to mpgl function
 #--------------------------------------------------------------------#
-vcf_to_mpgl <- compiler::cmpfun(function(vcffile, single_file, args){
+vcf_to_mpgl <- compiler::cmpfun(function(vcf_file){
   
-  print("Creating input genotype likelihood data for entropy...")
+  print("Creating input genotype likelihood data for entropy.")
   
-  fname <- strsplit(basename(vcffile), ".vcf")[[1]]
-  a.vcf <- read.vcfR(vcffile)
+  # parse out the simple name for the input VCF
+  simple_name <- vcf_file %>%
+    basename() %>%
+    str_remove_all(., ".vcf")
   
-  a.ids <- colnames(a.vcf@gt)[-1]
-  # write.table(a.ids, file=paste0("inds_", fname, ".txt"),
-  #             row.names = FALSE, col.names = FALSE)
+  # bring the variant data into memory as a vcfR object
+  vcf_data <- read.vcfR(vcf_file)
+  
+  # parse out the sample IDs and the varying loci
+  sample_ids <- colnames(vcf_data@gt)[-1]
+  sample_size <- length(sample_ids)
+  loci <- paste(vcf_data@fix[, 'CHROM'],
+                vcf_data@fix[, 'POS'],
+                sep="_")
+  n_loci <- length(loci)
+  
+  # Extract phred-scaled likelihoods, equivalent to:
+  # PL = -10 * log{P(Genotype | Data)}
+  sample_pl <- extract.gt(vcf_data, element='PL')
+  
+  # Extract sample size and variant count to get the dimensionality of the data
+  sample_size <- ncol(sample_pl)
+  variant_count <- nrow(sample_pl)
+  print(
+    paste(sample_size, "samples among", variant_count, "variants.", sep = " ")
+  )
+  dimensionality <- sample_size * variant_count
+  
+  # Calculate the percentage of possible data points that are missing
+  percent_missing <- sum(is.na(sample_pl)) * 100
+  print(
+    paste(
+      round(dimensionality / dimensionality, 1),
+      "% missing data in file",
+      sep = ""
+    )
+  )
+  
+  # convert phred-likelihoods into numeric vectors for every item in the matrix
+  sample_gl <- apply(sample_pl, c(1, 2), function(x) { to_numvec(x) })
+  
+  # run some checks to make sure conversions worked
+  stopifnot(sample_size == dim(sample_pl)[2])
+  stopifnot(n_loci == dim(sample_pl)[1])
+  
+  # create a vector with a ploidy value for each sample. Here we assume samples
+  # are all diploid.
+  ploidy <- rep(2, sample_size)
+  
   # 
-  a.loci <- paste0(a.vcf@fix[, 'CHROM'], ';', a.vcf@fix[, 'POS'])
-  
-  a.pl <- extract.gt(a.vcf, element='PL')
-  
-  print(paste0(round(sum(is.na(a.pl))*100/(nrow(a.pl)*ncol(a.pl)), 1),
-               "% missing data in file"))
-  
-  if (single_file){
-    
-    a.gl <- apply(a.pl, c(1, 2),
-                  function(x){
-                    as.numeric(trimws(unlist(strsplit(x, split=","))))
-                  })
-    
-    nind <- dim(a.pl)[2]
-    nloci <- dim(a.pl)[1]
-    ploidy <- rep(2, nind)
-    
-    print(paste0("Number of inds: ", nind, "    Number of loci: ", nloci)) 
-    
-    for(i in 1:nrow(a.pl)){
-      for(j in 1:ncol(a.pl)){
-        if(is.na(a.gl[1, i, j])){
-          a.gl[, i, j] <- rep(0, ploidy[[1]]+1)
-        }
+  for (i in 1:nrow(sample_pl)) {
+    for (j in 1:ncol(sample_pl)) {
+      if (is.na(sample_gl[1, i, j])) {
+        sample_gl[, i, j] <- rep(0, ploidy[[1]]+1)
       }
     }
-    unlink(paste0(fname, ".mpgl"))
-    cat(paste(nind, nloci, "\n"), file=paste0(fname, ".mpgl"))
-    cat(paste(a.ids), file=paste0(fname, ".mpgl"), append=T)
-    for(locus in 1:nloci){
-      cat(paste0("\n", a.vcf@fix[locus, 'CHROM'], " "),
-          file=paste0(fname, ".mpgl"), append = TRUE)
-      write.table(unlist(t(a.gl[, locus, ])), file=paste0(fname, ".mpgl"),
-                  append = TRUE, col.names = FALSE, row.names = FALSE, eol=" ")
-    }
-    
-  } else {
-    
-    # run multifile function
-    multifile_convert(args, a.pl, a.loci, a.ids)
-    
+  }
+  
+  # prepare output file
+  output_name <- paste(simple_name, "mpgl", sep = ".")
+  if (file.exists(output_name)) {
+    unlink(paste(simple_name, ".mpgl"))
+  }
+  
+  # prepare the header for the MPGL output file
+  cat(paste(sample_size, n_loci, "\n"), file = output_name)
+  cat(paste(sample_ids), file = output_name, append = T)
+  
+  # append the rows of likelihoods for each locus to complete the file
+  for (i in 1:n_loci) {
+    cat(paste0("\n", vcf_data@fix[i, 'CHROM'], " "),
+        file = output_name, append = TRUE)
+    write.table(unlist(t(sample_gl[, i, ])), file = output_name,
+                append = TRUE, col.names = FALSE, row.names = FALSE, eol=" ")
   }
   
 })
@@ -140,20 +117,11 @@ main <- compiler::cmpfun(function(){
   
   # retrieve positional command line arguments
   args <- commandArgs(TRUE)
-  
-  ## flag to check if we have mixed ploidy 
-  single_file <- TRUE
-  vcffile <- as.character(args[1])
-  if (!file.exists("ploidy_inds.txt")) {
-    cat("Assuming diploidy for each indv. in vcf \n")
-    #quit()
-  }
-  if (length(args)>2) {
-    single_file <- FALSE
-  }
+  vcf_file <- args[1]
+  stopifnot(file.exists(vcf_file))
   
   # run the conversion, which involves a nesting of the above functions
-  vcf_to_mpgl(vcffile, single_file, args)
+  vcf_to_mpgl(vcf_file)
   
 })
 #--------------------------------------------------------------------#
---
+++ bin/vcf2mpgl.jl
@@ -0,0 +1,170 @@
+#!/usr/bin/env -S julia --threads auto --gcthreads 3
+
+using Base.Threads, VariantCallFormat, VCFTools, CSV, DataFrames, Revise
+
+
+"""
+The struct `DatasetInfo` holds metadata about the VCF-formatted 
+dataset as a whole, including the number of markers (L), the sample
+size (N), a 1-dimensional array of sample IDs, and the ploidy of
+the species represented.
+"""
+struct DatasetInfo
+    L::Int64
+    N::Int64
+    Sample_IDs::Vector{String}
+    Ploidy::Int64
+end
+
+
+"""
+The struct `EntropyHeader` is a simple container for the first and
+and second header lines expected by Entropy in the MPGL format. The
+first line is the sample size and the number of markers, and the second
+line is the list of sample IDs. As with the rest of the file format,
+all elements are space-delimited.
+"""
+struct EntropyHeader
+    line1::String
+    line2::String
+end
+
+
+"""
+Take the VCF file and process it so that it can be rendered as
+a Julia dataframe.
+"""
+function to_mpgl_df(vcf_file::String, vcf_meta::DatasetInfo)::DataFrame
+
+    # create a sample data frame to fill with the correct PL values
+    mpgl_df = DataFrame(fill("", (vcf_meta.L, vcf_meta.N)), :auto)
+    rename!(mpgl_df, vcf_meta.Sample_IDs)
+
+    # prepare to collect unique locus identifiers
+    chrom = Vector{String}(undef, vcf_meta.L)
+    mpgl_df.chrom = chrom
+
+    # iterate through each record and extract the information needed for
+    # mpgl format
+    vcf_reader = VCF.Reader(open(vcf_file, "r"))
+    @inbounds for (i, record) in enumerate(vcf_reader)
+        # fill in the current marker's identifier
+        mpgl_df.chrom[i] = VCF.chrom(record)
+
+        # parse out genotypes, double checking that enough PL's are present
+        genotypes = VCF.genotype(record, :, "PL")
+
+        # fill in the correct genotype
+        for (j, genotype) in enumerate(genotypes)
+            mpgl_df[i,j] = replace(genotype, "," => " ")
+        end
+    end
+
+    # reorder so that chroms are first
+    select!(mpgl_df, :chrom, :)
+
+    return mpgl_df
+end
+
+
+"""
+Collect information about the VCF so that a dataframe with MPGL
+formatted information, along with the required header, can be
+compiled.
+"""
+function vcf_to_mpgl(vcf_file::String, ploidy::Int64)
+
+    # check that the VCF exists and log out
+    @assert isfile(vcf_file) "Provided VCF path, '$vcf_file', does not exist in the working directory."
+    println("Formatting input genotype likelihood data for the Entropy program from VCF $vcf_file")
+
+    # parse out the sample IDs and the varying loci, along with counts
+    sample_ids = sampleID(vcf_file)
+    sample_size = length(sample_ids)
+    n_loci = nrecords(vcf_file)
+
+    # initialize the structs to bundle data
+    vcf_meta = DatasetInfo(
+        n_loci,
+        sample_size, 
+        sample_ids,
+        ploidy,
+    )
+    header = EntropyHeader(
+        "$sample_size $n_loci",
+        join(sample_ids, " ")
+    )
+
+    # get the Phred-scaled genotype likelihoods as a dataframe
+    mpgl_df = to_mpgl_df(vcf_file, vcf_meta)
+
+    return (header, mpgl_df)
+
+end
+
+
+"""
+Write out the MPGL header and dataframe.
+"""
+function write_mpgl(header::EntropyHeader, mpgl_df::DataFrame, name_prefix::String)
+
+    # construct the output name based on the input prefix
+    output_name = "$name_prefix.mpgl"
+
+    # start with the header
+    open("tmp.mpgl", "w") do writer
+        println(writer, header.line1)
+        println(writer, header.line2)
+    end
+
+    # finish with the table data
+    open("tmp.mpgl", "a") do appender
+        CSV.write(
+            appender, mpgl_df;
+            append=true, delim=' ', quotestrings=false, writeheader=false
+        )
+    end
+
+    # remove quotes
+    open("tmp.mpgl", "r") do reader
+        open(output_name, "w") do writer
+            for line in eachline(reader)
+                println(writer, replace(line, "\"" => ""))
+            end
+        end
+    end
+    rm("tmp.mpgl")
+
+end
+
+
+"""
+Script entrypoint
+"""
+function convert_all_files()
+
+    # search for VCF files that can be converted
+    workingdir = length(ARGS) == 1 ? ARGS[1] : "."
+    vcf_files = [file for file in readdir(workingdir; join=true) if occursin(".vcf", file)]
+
+    # double check that VCF files were actually found
+    @assert length(vcf_files) > 0 "No VCF files found in the provided working directory"
+
+    # sort by size so that the smallest comes first, thereby limiting first-
+    # compilation time
+    vcf_sizes = [filesize(file) for file in vcf_files]
+    vcf_files = vcf_files[sortperm(vcf_sizes)]
+
+    # process each available VCF
+    Threads.@threads for vcf_file in vcf_files
+        # convert to MPGL dataframe
+        header, mpgl_df = vcf_to_mpgl(vcf_file, 2)
+
+        # parse out the input file simple name
+        simple_name = replace(vcf_file, ".vcf" => "")
+
+        # write out the new file
+        write_mpgl(header, mpgl_df, simple_name)
+    end
+
+end ; convert_all_files()
